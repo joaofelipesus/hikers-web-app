b17dc62a0b38a446431b5b3b43c8a982306a239a
joaofelipe <joaofelipe1294@gmail.com>
2025-10-11 07:15:09 -0300

    Big bang

---
+++ app/controllers/configurations_controller.rb
@@ -0,0 +1,26 @@
+class ConfigurationsController < ApplicationController
+  def ios_v1
+  end
+
+  def android_v1
+    render json: {
+      settings: {},
+      rules: [
+        {
+          patterns: [".*"],
+          properties: {
+            uri: "hotwire://fragment/web",
+            pull_to_refresh_enabled: true
+          }
+        },
+        {
+          patterns: ["/new$", "/edit$"],
+          properties: {
+            context: "modal",
+            pull_to_refresh_enabled: false
+          }
+        }
+      ]
+    }
+  end
+end
